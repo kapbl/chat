c5779b8f42b3a211facb2428168c19d8007dbdd5
tisoyim <tisoyim@163.com>
2025-04-23 22:23:59 +0800

    s

--- handles/login.go
+++ handles/login.go
@@ -23,19 +23,22 @@ func GenerateSHA256UserID(email string) string {
 
 func Login(ctx echo.Context) error {
 	loginData := struct {
+		Usernmae string `json:"username"`
 		Email    string `json:"email"`
 		Password string `json:"password"`
 	}{}
 	if err := ctx.Bind(&loginData); err != nil {
 		return ctx.JSON(400, map[string]string{"error": "请求格式错误"})
 	}
+	if loginData.Email == "" || loginData.Password == "" {
+		return ctx.JSON(400, map[string]string{"error": "邮箱和密码不能为空"})
+	}
 	// 在数据库中查找这个邮箱地址的用户
 	// 通过邮箱查询
-
 	var user models.User
 	if err := database.DB.Where("email = ? AND password = ?", loginData.Email, loginData.Password).First(&user).Error; err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return ctx.JSON(401, map[string]string{"error": "邮箱或密码错误"})
+			return ctx.JSON(401, map[string]string{"error": "邮箱和用户名或密码错误"})
 		}
 	}
 	return ctx.JSON(200, map[string]string{"message": "登录成功"})
